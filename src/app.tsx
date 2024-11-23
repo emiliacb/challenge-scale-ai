a6bf4cf21b97ace8250852cc9c5e6bd78f30d452
em3c2 <emiliacabralb@gmail.com>
2024-11-23 14:24:26 -0300

    basic three implementation from docs

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from 'react-dom/client';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas>
+      <OrbitControls />
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color="orange" />
+      </mesh>
+      </Canvas>
     </div>
   );
 };
