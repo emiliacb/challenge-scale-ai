0f9e4907e79cc00fcfce62b55f3fa48d76c5091e
em3c2 <emiliacabralb@gmail.com>
2024-11-23 14:51:56 -0300

    simple example of points

--- src/app.tsx
+++ src/app.tsx
@@ -1,25 +1,36 @@
 import React from "react";
-import { createRoot } from 'react-dom/client';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { createRoot } from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Points } from "@react-three/drei";
+
+const positions = new Float32Array([
+  -1, 1, 0, 1, 1, 0, 1, -1, 0, -1, -1, 0, 0, 0, 1, 0, 0, -1,
+]);
+
+const colors = new Float32Array([
+  1, 0, 0, 1, 0.5, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0,
+]);
+
+const sizes = new Float32Array([5, 5, 5, 5, 5, 5, 5, 5]);
 
 const App = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-      <OrbitControls />
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color="orange" />
-      </mesh>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <Canvas camera={{ position: [0, 0, 100], fov: 75 }}>
+        <OrbitControls />
+        <ambientLight intensity={0.5} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          intensity={1}
+        />
+        <Points positions={positions} colors={colors} sizes={sizes} />
       </Canvas>
     </div>
   );
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(<App />);
